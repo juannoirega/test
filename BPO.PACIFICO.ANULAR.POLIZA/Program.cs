1eaf98dfd57696868f2bd85ba346576b3106e1b2
Kayo <kribeirs@everis.com>
2018-08-08 10:22:59 -0300

    commit de anulação de poliza(não desarollado por mi pero que estas en commit para se adicionar)

---
+++ BPO.PACIFICO.ANULAR.POLIZA/Program.cs
@@ -0,0 +1,49 @@
+﻿using everis.Ees.Proxy.Core;
+using everis.Ees.Proxy.Services.Interfaces;
+using Everis.Ees.Entities;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BPO.PACIFICO.ANULAR.POLIZA
+{
+    class Program : IRobot
+    {
+        private static BaseRobot<Program> _robot = null;
+        private static IWebDriver _driverGlobal = null;
+        private static IWebElement element;
+
+
+        #region ParametrosRobot
+        private string _url = string.Empty;
+        private string _usuario = string.Empty;
+        private string _contraseña = string.Empty;
+        #endregion
+        #region VariablesGLoables
+        private static string _producto = string.Empty;
+        private static string _inicioVigencia = string.Empty;
+        private static string _finVigencia = string.Empty;
+        private static string _numeroAgente = string.Empty;
+        private static string _agente = string.Empty;
+        private static string _tipo = string.Empty;
+        private static string _estado = string.Empty;
+        private static string _tipoVigencia = string.Empty;
+        private static string _numeroCanal = string.Empty;
+
+        #endregion
+        static void Main(string[] args)
+        {
+        }
+
+        protected override void Start()
+        {
+        }
+    }
+}
