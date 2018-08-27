67d4e732e3e1f149ac8c1253950716b5a4a7abe5
Lazaro Mannucci <crlazaro@icloud.com>
2018-08-27 17:57:17 -0500

    BPO.PACIFICO.ACTUALIZAR.DATOS.CLIENTE

---
+++ BPO.PACIFICO.ACTUALIZAR.DATOS.CLIENTE/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPO.PACIFICO.ACTUALIZAR.DATOS.CLIENTE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
