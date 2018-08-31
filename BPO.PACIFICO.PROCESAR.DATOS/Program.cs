a384008370b32d99ab6dea2575ae3cd9cca02e37
aninamac <alejandro.nina.macedo@everis.com>
2018-08-31 16:59:47 -0500

    Modificaciones Robot Procesamiento de Datos.

--- BPO.PACIFICO.PROCESAR.DATOS/Program.cs
+++ BPO.PACIFICO.PROCESAR.DATOS/Program.cs
@@ -20,7 +20,7 @@ namespace BPO.PACIFICO.PROCESARDATOS.AP
         private static int _nDiasArrepentimiento;
         private static int _nDiasDesistimiento;
         private static string _cLineaPersonal;
-        private static string _cCanal;
+        private static string _cCanalVTAR_BCP;
         private static string _cPolizaEmision;
         private static string _cPolizaRenovacion;
         private static string _cEstadoAnulacion;
@@ -32,6 +32,10 @@ namespace BPO.PACIFICO.PROCESARDATOS.AP
         private static string _cRehabilitacionLLPP = string.Empty;
         private static string _cRehabilitacionAlianzas = string.Empty;
         private static string _cRehabilitacionRRGG = string.Empty;
+        private static bool _bEnviarNotificacion;
+        private static string _cDominioLineas = string.Empty;
+        private static string _cDominioProcesos = string.Empty;
+        private static string _cDominioLineasCol3 = string.Empty;
         private static StateAction _oMesaControl;
         private static StateAction _oPantallaValidacion;
         private static StateAction _oNotificacion;
@@ -59,7 +63,7 @@ namespace BPO.PACIFICO.PROCESARDATOS.AP
             foreach (Ticket oTicket in _oRobot.Tickets)
             {
                 try
-                {                    
+                {
                     _cLinea = _Funciones.ObtenerValorDominio(oTicket, Convert.ToInt32(oTicket.TicketValues.FirstOrDefault(a => a.FieldId == eesFields.Default.tipo_de_linea).Value)).ToUpperInvariant();
                     _oMesaControl = _oRobot.GetNextStateAction(oTicket).First(a => a.DestinationStateId == _nIdMesaControl);
                     _oPantallaValidacion = _oRobot.GetNextStateAction(oTicket).First(a => a.DestinationStateId == _nIdPantallaValidacion);
@@ -84,9 +88,9 @@ namespace BPO.PACIFICO.PROCESARDATOS.AP
                 _nDiasArrepentimiento = Convert.ToInt32(_oRobot.GetValueParamRobot("DiasArrepentimiento").ValueParam);
                 _nDiasDesistimiento = Convert.ToInt32(_oRobot.GetValueParamRobot("DiasDesistimiento").ValueParam);
                 _cLineaPersonal = _oRobot.GetValueParamRobot("LineaPersonal").ValueParam;
-                _cCanal = _oRobot.GetValueParamRobot("Canal").ValueParam;
+                _cCanalVTAR_BCP = _oRobot.GetValueParamRobot("CanalVTAR_BCP").ValueParam;
                 _cPolizaEmision = _oRobot.GetValueParamRobot("Poliza_Tipo_1").ValueParam;
-                _cPolizaRenovacion = _oRobot.GetValueParamRobot("Poliza_Tipo_2").ValueParam;               
+                _cPolizaRenovacion = _oRobot.GetValueParamRobot("Poliza_Tipo_2").ValueParam;
                 _cEstadoAnulacion = _oRobot.GetValueParamRobot("EstadoAnulacion").ValueParam;
                 _nIdMesaControl = Convert.ToInt32(_oRobot.GetValueParamRobot("EstadoError").ValueParam);
                 _nIdPantallaValidacion = Convert.ToInt32(_oRobot.GetValueParamRobot("EstadoSiguiente").ValueParam);
@@ -95,11 +99,12 @@ namespace BPO.PACIFICO.PROCESARDATOS.AP
                 _cRehabilitacionLLPP = _oRobot.GetValueParamRobot("LineaLLPP").ValueParam;
                 _cRehabilitacionAlianzas = _oRobot.GetValueParamRobot("LineaAlianzas").ValueParam;
                 _cRehabilitacionRRGG = _oRobot.GetValueParamRobot("LineaRRGG").ValueParam;
+                _bEnviarNotificacion = Convert.ToBoolean(_oRobot.GetValueParamRobot("EnviarNotificacion").ValueParam);
+                _cDominioLineas = _oRobot.GetValueParamRobot("DominioLineas").ValueParam;
+                _cDominioProcesos = _oRobot.GetValueParamRobot("DominioProcesos").ValueParam;
+                _cDominioLineasCol3 = _oRobot.GetValueParamRobot("DominioLineas_col3").ValueParam;
             }
-            catch (Exception Ex)
-            {
-                LogFailStep(12, Ex);
-            }
+            catch (Exception Ex) { LogFailStep(12, Ex); }
         }
 
         //Envía el ticket al siguiente estado:
@@ -119,9 +124,7 @@ namespace BPO.PACIFICO.PROCESARDATOS.AP
         {
             try
             {
-                var oContainer = ODataContextWrapper.GetContainer();
-                List<DomainValue> oDomainValues = new List<DomainValue>();
-                oDomainValues = oContainer.DomainValues.Where(a => a.DomainId == eesFields.Default.tipo_de_linea).ToList();
+                FunctionalDomains<List<DomainValue>> objLineas = _Funciones.GetDomainValuesByParameters(_oRobot.SearchDomain, _cDominioLineas, new string[,] { { _cDominioLineasCol3, oTicketDatos.TicketValues.FirstOrDefault(a => a.FieldId == eesFields.Default.tipo_proceso).Value} });
             }
             catch (Exception Ex) { throw new Exception("Ocurrió un error al obtener Línea de Negocio: " + Ex.Message, Ex); }
         }
@@ -228,7 +231,7 @@ namespace BPO.PACIFICO.PROCESARDATOS.AP
                                             - Convert.ToDateTime(oTicketDatos.TicketValues.FirstOrDefault(o => o.FieldId == eesFields.Default.date_inicio_vigencia).Value);
 
                     //Con devolución al 100%: 
-                    oTicketDatos.TicketValues.Add(new TicketValue { ClonedValueOrder = null, TicketId = oTicketDatos.Id, FieldId = eesFields.Default.aplica_prorrata, Value = "false" });
+                    oTicketDatos.TicketValues.Add(new TicketValue { ClonedValueOrder = null, TicketId = oTicketDatos.Id, FieldId = eesFields.Default.aplica_prorrata, Value = "0" });
 
                     //VERIFICA QUE SEA EMISIÓN:
                     if (oTicketDatos.TicketValues.FirstOrDefault(o => o.FieldId == eesFields.Default.tipo_poliza).Value.ToUpperInvariant() == _cPolizaEmision)
@@ -239,7 +242,7 @@ namespace BPO.PACIFICO.PROCESARDATOS.AP
                             //if (oTicketDatos.TicketValues.FirstOrDefault(o => o.FieldId == eesFields.Default.vobo_producto).Value.Length <= 0)
                             //{
                             //Con prorrateo:
-                            oTicketDatos.TicketValues.Add(new TicketValue { ClonedValueOrder = null, TicketId = oTicketDatos.Id, FieldId = eesFields.Default.aplica_prorrata, Value = "true" });
+                            oTicketDatos.TicketValues.Add(new TicketValue { ClonedValueOrder = null, TicketId = oTicketDatos.Id, FieldId = eesFields.Default.aplica_prorrata, Value = "1" });
                             //}
                         }
                     }
@@ -252,7 +255,7 @@ namespace BPO.PACIFICO.PROCESARDATOS.AP
                             //if (oTicketDatos.TicketValues.FirstOrDefault(o => o.FieldId == eesFields.Default.vobo_producto).Value.Length <= 0)
                             //{
                             //Con prorrateo:
-                            oTicketDatos.TicketValues.Add(new TicketValue { ClonedValueOrder = null, TicketId = oTicketDatos.Id, FieldId = eesFields.Default.aplica_prorrata, Value = "true" });
+                            oTicketDatos.TicketValues.Add(new TicketValue { ClonedValueOrder = null, TicketId = oTicketDatos.Id, FieldId = eesFields.Default.aplica_prorrata, Value = "1" });
                             //}
                         }
                     }
